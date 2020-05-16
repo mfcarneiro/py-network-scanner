3c5bbcfeca7c05394cea4c45442c5ac97783f32b
mfcarneiro <matheus.felipe123@gmail.com>
2020-05-16 19:43:21 -0300

    Refactor the display_scan_results method

--- network_scanner.py
+++ network_scanner.py
@@ -1,23 +1,43 @@
-#! /user/bin/env python3
+#! /usr/bin/env python3
 
+import argparse
 import scapy.all as scapy
 
 
+def init_banner():
+    arguments = get_user_arguments()
+    scanner_result = scan(arguments.target)
+    display_scan_results(scanner_result)
+
+
+def get_user_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target',
+                        help='IP of the target')
+    arguments = parser.parse_args()
+
+    return arguments
+
+
 def scan(ip):
+    client_list = []
     arp_request = scapy.ARP(pdst=ip)
     # Using srp because of this custom MAC Address
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(
-        arp_request_broadcast, timeout=1)[0]
+        arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    display_scan_results(answered_list)
+    for result in answered_list:
+        answered_dictionary = {'ip': result[1].psrc, 'mac': result[1].hwsrc}
+        client_list.append(answered_dictionary)
+
+    return client_list
 
 
 def display_scan_results(result_list):
     print('IP\t\t\tMAC Address\n-----------------------------------')
-    for result in result_list:
-        print(result[1].psrc + '\t\t' + result[1].hwsrc)
 
+    for client in result_list:
+        print(client['ip'] + '\t\t' + client['mac'])
 
-scan("192.168.1.0/24")
